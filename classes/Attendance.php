d5978d0fcb51961aa8f3ddf28eb3b24ab2f4f670
Brahmia Lokman Abdelmoname <brahmia.lokmeneabdelmoname@univ-guelma.dz>
2025-04-18 22:41:51 +0100

    finish attendance page in teacher dashboard

--- classes/Attendance.php
+++ classes/Attendance.php
@@ -11,17 +11,32 @@ class Attendance
     private $group_id;
     private $db;
 
-    public function __construct($user_id, $db)
+    public function __construct($group_id, $db)
     {
-        $this->user_id = $user_id;
+        $this->group_id = $group_id;
         $this->db = $db;
     }
 
     public function getAttendance()
     {
         try {
-            $query = $this->db->prepare("SELECT * FROM attendance WHERE user_id=?");
-            $query->execute([$this->user_id]);
+            $query = $this->db->prepare("
+            SELECT 
+                users.first_name,
+                users.last_name, 
+                students.parent_phone, 
+                students.id, 
+                attendance.status, 
+                attendance.date, 
+                attendance.note,
+                groups.group_name
+            FROM attendance
+            JOIN students ON students.id = attendance.student_id
+            JOIN users ON users.id = students.user_id
+            JOIN groups ON groups.id = attendance.group_id
+            WHERE attendance.group_id = ?
+        ");
+            $query->execute([$this->group_id]);
             $user = $query->fetchAll(PDO::FETCH_ASSOC);
             return $user;
         } catch (PDOException $e) {
